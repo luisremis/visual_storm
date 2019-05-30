dec448d2056032f7119ab6bedce9cca4452bb478
Lacewell, Chaunte W <chaunte.w.lacewell@intel.com>
2019-05-29 22:41:55 -0700

    Updated to ingest using threads and batches

--- yfcc100m/python/build_yfcc_db.py
+++ yfcc100m/python/build_yfcc_db.py
@@ -12,13 +12,15 @@ import vdms
 def get_args():
     parserobj = argparse.ArgumentParser()
     parserobj.add_argument('-num_threads', type=int, default=64,
-                           help='Number of threads to use [default: 100]')
+                           help='Number of threads to use [default: 64]')
+    parserobj.add_argument('-batch_size', type=int, default=1,
+                           help='Number of entries per thread [default: 1]')
     parserobj.add_argument('-data_file', type=str,
                            default='/data/yfcc100m/set_0/data_0/metadata/yfcc100m_short/yfcc100m_dataset_short',
                            help='YFCC metadata [default: ' +
                                 '/data/yfcc100m/set_0/data_0/metadata/yfcc100m_short/yfcc100m_dataset_short')
-    parserobj.add_argument('-tag_list', type=str, default='autotag_list.txt',
-                           help='List of expected tags [default: autotag_list.txt]')
+    parserobj.add_argument('-tag_list', type=str, default='../yfcc_parse_labels/autotag_list.txt',
+                           help='List of expected tags [default: ../yfcc_parse_labels/autotag_list.txt]')
     parserobj.add_argument('-tag_file', type=str,
                            default='/data/yfcc100m/set_0/data_0/metadata/yfcc100m_short/yfcc100m_autotags_short',
                            help='YFCC file of autotags [default: ' +
@@ -54,114 +56,146 @@ def disconnect_db_list(dbs):
         db.disconnect()
 
 
-def process_tag_entities(params, dbs, tags):
-    num_tags = len(tags)
-    retries = int(np.ceil(num_tags / params.num_threads))
-    results = [None] * num_tags
+def make_logger(name, log_file, level=logging.INFO):
+    logger = logging.getLogger(name)
+    # format = logging.Formatter(logging.BASIC_FORMAT)
+    format = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    handler = logging.FileHandler(log_file, mode='w')        
+    handler.setFormatter(format)
+    
+    streamhandler = logging.StreamHandler()
+    streamhandler.setFormatter(format)
 
-    for ret in range(0, retries):
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    logger.addHandler(streamhandler)
+
+    return logger
+    
+    
+def process_tag_entities(params, dbs, tags):
+    batch = params.batch_size
+    num_lines = len(tags)
+    blocks = int(np.ceil(num_lines / (params.num_threads * batch)))
+    results = [None] * num_lines
+
+    for block in range(0, blocks):
         thread_arr = []
+        
         for i in range(0, params.num_threads):
-            idx = params.num_threads * ret + i
-            if idx < num_tags:
-                thread_add = Thread(target=util.add_autotags_entity, args=(idx, dbs[i], tags[idx], results))
+            idx = params.num_threads * block + i * batch
+            
+            if idx < num_lines:
+                thread_add = Thread(target=util.add_autotags_entity_batch, args=(idx, dbs[i], tags[idx:min([idx+batch, num_lines])], results))
                 thread_add.start()
                 thread_arr.append(thread_add)
-        idx = params.num_threads * ret
+            else:
+                break
+        
+        idx = params.num_threads * block
         error_counter = 0
         for th in thread_arr:
             th.join()
-            if results[idx] == -1:
-                error_counter += 1
-                logging.debug('\t[!] [process_tag_entities] Index {} status: {}'.format(idx, results[idx]))
-            idx += 1
+            error_counter += results[idx:min([idx+batch, num_lines])].count(-1)
+            idx += batch
+    # return results, error_counter
+    return error_counter
 
 
 def process_image_entities(params, dbs, yfcc_data):
+    batch = params.batch_size
     num_lines = len(yfcc_data)
-    retries = int(np.ceil(num_lines / params.num_threads))
+    blocks = int(np.ceil(num_lines / (params.num_threads * batch)))
     results = [None] * num_lines
 
-    for ret in range(0, retries):
+    for block in range(0, blocks):
         thread_arr = []
+        
         for i in range(0, params.num_threads):
-            idx = params.num_threads * ret + i
+            idx = params.num_threads * block + i * batch
+            
             if idx < num_lines:
-                thread_add = Thread(target=util.add_image_entity, args=(idx, dbs[i], yfcc_data.iloc[idx, :], results))
+                thread_add = Thread(target=util.add_image_entity_batch, args=(idx, min([idx+batch, num_lines]), dbs[i], yfcc_data.iloc[idx:min([idx+batch, num_lines]), :], results))
                 thread_add.start()
                 thread_arr.append(thread_add)
-        idx = params.num_threads * ret
+            else:
+                break
+        
+        idx = params.num_threads * block
         error_counter = 0
         for th in thread_arr:
             th.join()
-            if results[idx] == -1:
-                error_counter += 1
-                logging.debug('\t[!] [process_image_entities] Index {} status: {}'.format(idx, results[idx]))
-            idx += 1
+            error_counter += results[idx:min([idx+batch, num_lines])].count(-1)
+            idx += batch
+    return error_counter
 
 
 def process_connections(params, dbs, yfcc_data):
+    batch = 10
+    # batch = params.batch_size
     num_lines = len(yfcc_data)
-    retries = int(np.ceil(num_lines / params.num_threads))
+    blocks = int(np.ceil(num_lines / (params.num_threads * batch)))
     results = [None] * num_lines
 
-    for ret in range(0, retries):
+    for block in range(0, blocks):
         thread_arr = []
+        
         for i in range(0, params.num_threads):
-            idx = params.num_threads * ret + i
+            idx = params.num_threads * block + i * batch
+            
             if idx < num_lines:
-                thread_add = Thread(target=util.add_autotag_connection,
-                                    args=(idx, dbs[i], yfcc_data.iloc[idx, :], results))
+                thread_add = Thread(target=util.add_autotag_connection_batch,
+                                    args=(idx, dbs[i], yfcc_data.iloc[idx:min([idx+batch, num_lines]), :], results))
                 thread_add.start()
                 thread_arr.append(thread_add)
-        idx = params.num_threads * ret
+            else:
+                break
+                
+        idx = params.num_threads * block
         error_counter = 0
         for th in thread_arr:
             th.join()
-            if results[idx] == -1:
-                error_counter += 1
-                logging.debug('\t[!] [process_connections] Index {} status: {}'.format(idx, results[idx]))
-            idx += 1
+            error_counter += results[idx:min([idx+batch, num_lines])].count(-1)
+            idx += batch
+    return error_counter
 
 
 def main(in_args):
-    logging.debug('[!] Building database')
+    main_logger.info('[!] Building database')
     start = time.time()
 
     # Get data
     data, all_tags = get_data(in_args)
-    logging.debug('\t[!] [get_data] Total elapsed time: {:0.4f}'.format(time.time() - start))
+    e_time = time.time() - start
+    main_logger.info('\t[!] [get_data] Total elapsed time: {:0.4f} secs ({:0.4f} mins)'.format(e_time, e_time / 60.))
 
     db_list = get_db_list(in_args.num_threads)
 
     # Add Entities for Tags
     start_t = time.time()
-    process_tag_entities(in_args, db_list, all_tags)
-    logging.debug('\t[!] [process_tag_entities] Total elapsed time: {:0.4f}'.format(time.time() - start_t))
+    tag_num_errors = process_tag_entities(in_args, db_list, all_tags)
+    e_time = time.time() - start_t
+    main_logger.info('\t[!] [process_tag_entities] Total Errors: {}\tTotal elapsed time: {:0.4f} secs ({:0.4f} mins)'.format(tag_num_errors, e_time, e_time / 60.))
 
     # Add Entities for Metadata
     start_t = time.time()
-    process_image_entities(in_args, db_list, data)
-    logging.debug('\t[!] [process_image_entities] Total elapsed time: {:0.4f}'.format(time.time() - start_t))
+    image_num_errors = process_image_entities(in_args, db_list, data)
+    e_time = time.time() - start_t
+    main_logger.info('\t[!] [process_image_entities] Total Errors: {}\tTotal elapsed time: {:0.4f} secs ({:0.4f} mins)'.format(image_num_errors, e_time, e_time / 60.))
 
     # Add Connections between Tags and Metadata
     start_t = time.time()
-    process_connections(in_args, db_list, data)
-    logging.debug('\t[!] [process_connections] Total elapsed time: {:0.4f}'.format(time.time() - start_t))
+    connection_num_errors = process_connections(in_args, db_list, data)
+    e_time = time.time() - start_t
+    main_logger.info('\t[!] [process_connections] Total Errors: {}\tTotal elapsed time: {:0.4f} secs ({:0.4f} mins)'.format(connection_num_errors, e_time, e_time / 60.))
 
     disconnect_db_list(db_list)
-    logging.debug('[!] Total elapsed time: {:0.4f}'.format(time.time() - start))
+    e_time = time.time() - start
+    main_logger.info('[!] Total elapsed time: {:0.4f} secs ({:0.4f} mins)'.format(e_time, e_time / 60.))
 
 
 if __name__ == "__main__":
-    log_file = "build_yfcc_db.log"
-    logging.basicConfig(filename=str(log_file), filemode='w', level=logging.DEBUG)
-    stderrlogger = logging.StreamHandler()
-    stderrlogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
-    logging.getLogger().addHandler(stderrlogger)
+    main_logger = make_logger('main_logger', "build_yfcc_db_batching.log")
 
     args = get_args()
     main(args)
-
-    if stderrlogger:
-        logging.getLogger().removeHandler(stderrlogger)
--- yfcc100m/python/util.py
+++ yfcc100m/python/util.py
@@ -1,4 +1,6 @@
 import os.path
+import logging
+import numpy as np
 
 property_names = ['Line number', 'ID', 'Hash', 'User NSID',
                   'User nickname', 'Date taken', 'Date uploaded', 'Capture device',
@@ -8,6 +10,164 @@ property_names = ['Line number', 'ID', 'Hash', 'User NSID',
                   'Server ID', 'Farm ID', 'Secret', 'Secret original',
                   'Extension', 'Marker']
 
+
+def add_autotags_entity_batch(index, database, tag, results):
+    all_queries = []
+    for ix, row in enumerate(tag):
+        query = {}
+        add_entity = {"class": 'autotags', "properties": {"name": row}}
+        query["AddEntity"] = add_entity
+        all_queries.append(query)
+    res = database.query(all_queries)
+    # print('res[0][0]', res[0][0])
+    for b in range(0, len(tag)):
+        results[index + b] = res[0][b]['AddEntity']["status"]
+
+
+def add_image_entity_batch(index, index2, database, row_data, results):
+    from pandas import isna
+    from urllib.parse import urlparse
+    
+    def check_status(response):
+        redo = False
+        for ix, b in enumerate(range(index, index2)):
+            try:
+                results[b] = response[0][ix]['AddEntity']["status"]
+            except:
+                results[b] = -1
+                redo = True
+                break
+        return redo
+    
+    all_queries = []
+    for ix, row in row_data.iterrows():
+        # Set Metadata as properties
+        props = {}
+        for key in property_names:
+            if not isna(row[key]):
+                props[key] = int(row[key]) if key == 'ID' else str(row[key])
+        props["VD:imgPath"] = 'db/images/jpg' + urlparse(props['Download URL']).path
+        props["format"] = "jpg"
+
+        query = {}
+        add_entity = {"class": "VD:IMG", "properties": props}
+        query["AddEntity"] = add_entity
+        all_queries.append(query)
+    
+    redo_flag = False
+    cnt = 0
+    while redo_flag is True and cnt < 5:
+        res = database.query(all_queries)
+        redo_flag = check_status(res)
+        cnt += 1
+    # res = database.query(all_queries)
+    # redo = False
+    # for ix, b in enumerate(range(index, index2)):
+        # try:
+            # results[b] = res[0][ix]['AddEntity']["status"]
+        # except:
+            # results[b] = -1
+            # res = database.query(all_queries)
+            # redo = True
+            # break
+    # if redo:
+        # for ix, b in enumerate(range(index, index2)):
+            # try:
+                # results[b] = res[0][ix]['AddEntity']["status"]
+            # except:
+                # results[b] = -1
+
+
+def add_autotag_connection_batch(index, database, row_data, results):
+    import pandas as pd
+    
+    def check_status(response, indices):
+        redo = False
+        for rix, n in enumerate(indices):
+            tmp = response[n[0] : n[1]]
+            try:
+                for i in range(len(tmp)):
+                    cmd = list(tmp[i][0].items())[0][0]
+                    assert tmp[i][0][cmd]["status"] == 0
+                results[index + rix] = 0        
+            except:
+                results[index + rix] = -1
+                redo = True
+                break
+        return redo
+    
+    run_index = []
+    num_queries = 0
+    for rix, row in row_data.iterrows():
+        # Find Tag
+        if not pd.isna(row['autotags']):
+            ind = [None, None]
+            all_queries = []
+            # Find Image
+            parentref = 1000 * rix
+            query = {}
+            find_image = {'constraints': {'ID': ["==", int(row['ID'])]}, "_ref": parentref}
+            query["FindImage"] = find_image
+            all_queries.append(query)
+            ind[0] = num_queries
+            # run_index[rix,0] = num_queries
+            
+            num_queries +=1
+            
+            current_tags = row['autotags'].split(',')
+            for ix, t in enumerate(current_tags):
+                val = t.split(':')
+                this_ref = parentref + ix + 1
+
+                # Find Tag 
+                query = {}
+                find_entity = {"class": 'autotags', "_ref": this_ref, "constraints": {'name': ["==", val[0]]}}
+                query["FindEntity"] = find_entity
+                all_queries.append(query)
+                # run_index[rix,1] = num_queries
+                num_queries +=1
+
+                # Add Connection
+                query = {}
+                add_connection = {"class": 'tag', "ref1": parentref, "ref2": this_ref,
+                                  "properties": {"tag_name": val[0], 'tag_prob': float(val[1]),
+                                                 "MetaDataID": int(row['ID'])}}
+                query["AddConnection"] = add_connection
+                all_queries.append(query)
+                ind[1] = num_queries
+                # run_index[rix,1] = num_queries
+                num_queries +=1
+            run_index.append(ind)
+    
+    redo_flag = False
+    cnt = 0
+    while redo_flag is True and cnt < 5:
+        res = database.query(all_queries)
+        redo_flag = check_status(res, run_index)
+        cnt += 1
+    # redo = False
+    # for rix, n in enumerate(run_index):
+        # tmp = res[n[0] : n[1]]
+        # try:
+            # for i in range(len(tmp)):
+                # cmd = list(tmp[i][0].items())[0][0]
+                # assert tmp[i][0][cmd]["status"] == 0
+            # results[index + rix] = 0        
+        # except:
+            # results[index + rix] = -1
+            # res = database.query(all_queries)
+            # redo = True
+            # break
+    # if redo:
+        # for rix, n in enumerate(run_index):
+            # tmp = res[n[0] : n[1]]
+            # try:
+                # for i in range(len(tmp)):
+                    # cmd = list(tmp[i][0].items())[0][0]
+                    # assert tmp[i][0][cmd]["status"] == 0
+                # results[index + rix] = 0        
+            # except:
+                # results[index + rix] = -1
 
 def add_autotags_entity(index, database, tag, results):
     query = {}
@@ -26,9 +186,10 @@ def add_image_entity(index, database, row_data, results):
     props = {}
     for key in property_names:
         if not isna(row_data[key]):
+            # props[key] = str(row_data[key])
             props[key] = int(row_data[key]) if key == 'ID' else str(row_data[key])
     props["VD:imgPath"] = 'db/images/jpg' + urlparse(props['Download URL']).path
-    props["format"] = props['Extension']
+    props["format"] = "jpg"
 
     query = {}
     add_entity = {"class": "VD:IMG", "properties": props}
@@ -40,11 +201,12 @@ def add_image_entity(index, database, row_data, results):
 def add_autotag_connection(index, database, row_data, results):
     import pandas as pd
     all_queries = []
+    success_counter = 0
 
     # Find Image
     parentref = 10
     query = {}
-    find_image = {'constraints': {'ID': ["==", row_data['ID']]}, "_ref": parentref}
+    find_image = {'constraints': {'ID': ["==", int(row_data['ID'])]}, "_ref": parentref}
     query["FindImage"] = find_image
     all_queries.append(query)
 
@@ -73,10 +235,12 @@ def add_autotag_connection(index, database, row_data, results):
         for i in range(len(res)):
             cmd = list(res[i][0].items())[0][0]
             assert res[i][0][cmd]["status"] == 0, 0
+            # success_counter +=1
+        results[index] = 0
+        # results[index] = success_counter
+        
     except:
         results[index] = -1
-
-    results[index] = 0
 
 
 def display_images(imgs):
