9371e84a1cc025e2ab65fe33edc6d709d2a25cd6
Luis Remis <luis.remis@intel.com>
2019-06-10 06:32:49 -0700

    Add vdms config for yfcc100m

---
+++ yfcc100m/vdms/check_graph.sh
@@ -0,0 +1,7 @@
+/home/luisremi/vcs/pmgd/tools/dumpgraph db/graph > dump.graph
+echo "Number of Images:"
+cat dump.graph | grep "#VD:IMG" | wc -l
+echo "Number of autotags:"
+cat dump.graph | grep "#autotags" | wc -l
+echo "Number of connections:"
+cat dump.graph | grep "#tag:" | wc -l
