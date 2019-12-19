5022232e01518bf8175f370e5f567aeab57ceca4
Luis Remis <luis.remis@intel.com>
2019-12-18 21:30:00 -0800

    Updates on build scripts for vdms

--- yfcc100m/python/build_databases/vdms_build/build_all.sh
+++ yfcc100m/python/build_databases/vdms_build/build_all.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
-# rm error_*.log screen_*.log
+rm -rf logs
+mkdir logs
 
-# python3 build_yfcc_db.py \
-#           -data_file=/mnt/yfcc100m/metadata/processed/splitted/yfcc100m_dataset_000 \
-#           -tag_file=/mnt/yfcc100m/metadata/processed/splitted/yfcc100m_dataset_autotags_000 \
-#           -add_tags > screen_000.log 2> error_000.log
+input_root="/data/datasets/yfcc100m/metadata/processed/splitted/"
+output_root="/data/datasets/yfcc100m/golden_building/"
 
-#sync
-# (cd ../vdms/debug && bash check_graph.sh >> done.log && bsdtar cvfz /mnt/nvme1/vdms_archive_000.tar.gz db)
-#(cd /mnt/nvme1/vdms/ && bsdtar cvfz /mnt/nvme1/vdms_archive_000.tar.gz db)
+python3 build_yfcc_db.py \
+          -data_file=${input_root}/yfcc100m_dataset_000 \
+          -tag_file=${input_root}/yfcc100m_dataset_autotags_000 \
+          -add_tags > logs/screen_000.log 2> logs/error_000.log
 
-for i in $(seq -f "%03g" 51 100)
+sync
+(cd ${output_root}/vdms/ && \
+ bash check_graph.sh && \
+ bsdtar cvfz ${output_root}/vdms_archive_000.tar.gz db )
+
+for i in $(seq -f "%03g" 0 4)
 do
     echo "Building: "
-    echo /mnt/yfcc100m/metadata/processed/splitted/yfcc100m_dataset_$i
+    echo yfcc100m_dataset_$i
 
     python3 build_yfcc_db.py \
-        -data_file=/mnt/yfcc100m/metadata/processed/splitted/yfcc100m_dataset_$i \
-        -tag_file=/mnt/yfcc100m/metadata/processed/splitted/yfcc100m_dataset_autotags_$i \
-        > screen_$i.log 2> error_$i.log
+        -data_file=${input_root}/yfcc100m_dataset_$i \
+        -tag_file=${input_root}/yfcc100m_dataset_autotags_$i \
+        > logs/screen_$i.log 2> logs/error_$i.log
 
-    if [ "$i" -eq 10 ] || [ "$i" -eq 20 ] || [ "$i" -eq 50 ] || [ "$i" -eq 70 ] || [ "$i" -eq 90 ]
+    if [ "$i" -eq 0 ] || [ "$i" -eq 4 ] || [ "$i" -eq 9 ] || [ "$i" -eq 19 ] || [ "$i" -eq 49 ] || [ "$i" -eq 69 ] || [ "$i" -eq 89 ]
     then
         sync
         echo "checkpointing..."
-        (cd /mnt/nvme1/vdms/ && \
-         bsdtar cvfz /mnt/nvme1/vdms_archive_$i.tar.gz db)
+        (cd ${output_root}/vdms/ && \
+         bash check_graph.sh && \
+         bsdtar cvfz ${output_root}/vdms_archive_$i.tar.gz db)
     fi
-
-    # (cd ../vdms/debug/ && bash check_graph.sh >> done.log && bsdtar cvfz /mnt/nvme1/vdms_archive_$i.tar.gz db)
 
  done
--- yfcc100m/python/build_databases/vdms_build/run_vdms.sh
+++ yfcc100m/python/build_databases/vdms_build/run_vdms.sh
@@ -2,7 +2,7 @@ rm -rf db
 mkdir -p db/images
 # ln -s /data/yfcc100m/set_0/data_0/images db/images/jpg
 
-/home/luisremi/vcs/pmgd/tools/mkgraph db/graph -c 32 \
+/home/luisremi/vcs/pmgd/tools/mkgraph db/graph -c 64 \
 	node VD:IMG imgPath string \
 	node VD:IMG ID integer \
 	node VD:IMG Latitude float \
