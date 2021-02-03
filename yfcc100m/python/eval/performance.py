e9eefd304db781b71d26b3d44705c64c80540b1a
Luis Remis <luis.remis@intel.com>
2021-02-02 19:50:41 -0800

    Update performance experiment scripts

--- yfcc100m/python/eval/performance.py
+++ yfcc100m/python/eval/performance.py
@@ -16,10 +16,56 @@ VDMS_PORT_MAPPING = {'100k': 55500, '500k': 55405,
                      '10M':  55510, '50M':  55000,
                      '100M': 50000}
 
-SIZE_PROB_MAPPING = {'100k': 0.60,       '500k': 0.06,
-                     '1M':   0.92175,    '5M':   0.955,
-                     '10M':  0.972499,   '50M':  0.99375,
-                     '100M': 0.997656249}
+# SIZE_PROB_MAPPING = {'100k': 0.60,       '500k': 0.06,
+#                      '1M':   0.92175,    '5M':   0.955,
+#                      '10M':  0.952499,   '50M':  0.95375,
+#                      '100M': 0.967656249}
+
+# These are pre-computed probabilities so that queries returned
+# a controlled number of results, to prevent the throughput to be
+# affected by higher number of results
+PROB_MAPPING = {
+            '1M': {
+              "1tag":           0.735,
+              "1tag_loc20":     0.0625,
+              "2tag_loc20_or":  0.059375,
+              "2tag_or":        0.975,
+              "2tag_loc20_and": 0.06875,
+              "2tag_and":       0.059375,
+            },
+            '5M': {
+              "1tag":           0.949375,
+              "1tag_loc20":     0.0625,
+              "2tag_or":        0.984375,
+              "2tag_and":       0.59375,
+              "2tag_loc20_or":  0.71249,
+              "2tag_loc20_and": 0.06875,
+            },
+            '10M': {
+              "2tag_loc20_or":  0.8312499,
+              "2tag_and":       0.653125,
+              "1tag_loc20":     0.0625,
+              "2tag_or":        0.9875,
+              "2tag_loc20_and": 0.06875,
+              "1tag":           0.96468749
+            },
+            '50M':  {
+              "1tag":           0.995,
+              "2tag_and":       0.74218749,
+              "1tag_loc20":     0.0625,
+              "2tag_loc20_or":  0.95,
+              "2tag_loc20_and": 0.06875,
+              "2tag_or":        0.9953125
+            },
+            '100M': {
+              "1tag":           0.9975,
+              "1tag_loc20":     0.75,
+              "2tag_and":       0.8015625,
+              "2tag_loc20_or":  0.9781249,
+              "2tag_loc20_and": 0.06875,
+              "2tag_or":        0.997656249
+            }
+        }
 
 # These are operations using VDMS API format / params.
 OP_RESIZE_DOWN = {"type": "resize", "width": 224,  "height": 224}
@@ -28,72 +74,65 @@ OP_ROTATE      = {"type": "rotate", "angle": 45.0, "resize": False}
 # Different queries, each with its own parameters.
 # Database object must implement each of these queries
 
-METADATA_ONLY = True
+QUERY_PARAMS = [
+            # {'key': '1tag',
+            #  'tags': ["alligator"], 'probs': [0.99],
+            #  'operations': [],
+            #  'comptype': "and" },
+            # {'key': '1tag_loc20',
+            #  'tags': ["alligator"], 'probs': [0.4],
+            #  'lat': -14.354356, 'long': -39.002567,
+            #  'range_dist': 20,
+            #  'operations': [],
+            #  'comptype': "and"},
+            # {'key': '2tag_and',
+            #  'tags': ["alligator", "lake"], 'probs': [0.75, 0.75],
+            #  'comptype': "and" },
+            # {'key': '2tag_or',
+            #  'tags': ["alligator", "lake"], 'probs': [0.8, 0.8],
+            #  'comptype': "or" },
+            # {'key': '2tag_loc20_and',
+            #  'tags': ["alligator", "lake"], 'probs': [0.35, 0.35],
+            #  'lat': -14.354356, 'long': -39.002567,
+            #  'range_dist': 20,
+            #  'comptype': "and" },
+            # {'key': '2tag_loc20_or',
+            #  'tags': ["alligator", "lake"], 'probs': [0.90, 0.95],
+            #  'lat': -14.354356, 'long': -39.002567,
+            #  'range_dist': 20,
+            #  'comptype': "or" },
 
-if METADATA_ONLY:
-
-    QUERY_PARAMS = [
-                    {'key': '1tag',
-                     'tags': ["alligator"], 'probs': [1.1],
-                     'operations': [],
-                     'comptype': "or" },
-                    {'key': '1tag_loc20',
-                     'tags': ["alligator"], 'probs': [0.4],
-                     'lat': -14.354356, 'long': -39.002567,
-                     'range_dist': 20,
-                     'operations': [],
-                     'comptype': "or"  },
-                    {'key': '2tag_and',
-                     'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
-                     'comptype': "and" },
-                    {'key': '2tag_or',
-                     'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
-                     'comptype': "or" },
-                    {'key': '2tag_loc20_and',
-                     'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
-                     'lat': -14.354356, 'long': -39.002567,
-                     'range_dist': 20,
-                     'comptype': "and" },
-                    {'key': '2tag_loc20_or',
-                     'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
-                     'lat': -14.354356, 'long': -39.002567,
-                     'range_dist': 20,
-                     'comptype': "or" },
-                ]
-else:
-
-    QUERY_PARAMS = [
-                {'key': '1tag_resize',
-                 'tags': ["alligator"], 'probs': [1.1],
-                 'operations': [OP_RESIZE_DOWN],
-                 'comptype': "or" },
-                {'key': '1tag_loc20_resize',
-                 'tags': ["alligator"], 'probs': [0.4],
-                 'lat': -14.354356, 'long': -39.002567,
-                 'range_dist': 20,
-                 'operations': [OP_RESIZE_DOWN],
-                 'comptype': "or" },
-                {'key': '2tag_resize_and',
-                 'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
-                 'operations': [OP_RESIZE_DOWN],
-                 'comptype': "and" },
-                {'key': '2tag_resize_or',
-                 'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
-                 'operations': [OP_RESIZE_DOWN],
-                 'comptype': "or" },
-                {'key': '2tag_loc20_resize_and',
-                 'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
-                 'lat': -14.354356, 'long': -39.002567,
-                 'range_dist': 20,
-                 'operations': [OP_RESIZE_DOWN],
-                 'comptype': "and" },
-                {'key': '2tag_loc20_resize_or',
-                 'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
-                 'lat': -14.354356, 'long': -39.002567,
-                 'range_dist': 20,
-                 'operations': [OP_RESIZE_DOWN],
-                 'comptype': "or" },
-                  ]
+            {'key': '1tag_resize',
+             'tags': ["alligator"], 'probs': [1.1],
+             'operations': [OP_RESIZE_DOWN],
+             'comptype': "or" },
+            {'key': '1tag_loc20_resize',
+             'tags': ["alligator"], 'probs': [0.4],
+             'lat': -14.354356, 'long': -39.002567,
+             'range_dist': 20,
+             'operations': [OP_RESIZE_DOWN],
+             'comptype': "or" },
+            {'key': '2tag_resize_and',
+             'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
+             'operations': [OP_RESIZE_DOWN],
+             'comptype': "and" },
+            {'key': '2tag_resize_or',
+             'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
+             'operations': [OP_RESIZE_DOWN],
+             'comptype': "or" },
+            {'key': '2tag_loc20_resize_and',
+             'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
+             'lat': -14.354356, 'long': -39.002567,
+             'range_dist': 20,
+             'operations': [OP_RESIZE_DOWN],
+             'comptype': "and" },
+            {'key': '2tag_loc20_resize_or',
+             'tags': ["alligator", "lake"], 'probs': [0.95, 0.95],
+             'lat': -14.354356, 'long': -39.002567,
+             'range_dist': 20,
+             'operations': [OP_RESIZE_DOWN],
+             'comptype': "or" },
+              ]
 
 def reject_outliers(data, m = 2.):
     d = np.abs(data - np.median(data))
@@ -124,8 +163,8 @@ def get_args():
     # Run Config
     obj.add_argument('-numthreads', type=int, default=4,
                      help='Number of workers [default: 4]')
-    obj.add_argument('-numiters', type=int, default=10,
-                     help='Number of times to process all threads [default: 10]')
+    obj.add_argument('-numiters', type=int, default=2,
+                     help='Number of times to process all threads [default: 2]')
 
     # Output CSV
     obj.add_argument('-out_folder', type=str, default=None,
@@ -148,9 +187,13 @@ def get_args():
 
 def run_query_thread(obj, params, index, results, query_arguments):
 
+    start = time.time()
     # Here the object runs the query and returns time and len
     data_dict = obj.run_query(query_arguments)
     results[index].update(data_dict)
+
+    endtime = time.time() - start
+    print("query_thread took:", endtime) # Debugging
 
 def run_query(params, query_arguments):
 
@@ -188,34 +231,20 @@ def main(params):
     datafile = params.out_folder + "/data"
     evfw = EvalFramework.EvalFramework(datafile)
 
-    print('DATABASE: {} - N_THREADS: {}'.format(params.db_name,
+    print('ENGINE: {} - DB_SIZE: {} - N_THREADS: {}'.format(
+                                                params.db_type,
+                                                params.db_name,
                                                 params.numthreads))
     print("============================\n")
 
     for query_args in QUERY_PARAMS:
 
-        if query_args["key"] == "_1tag":
-            for i in range(len(query_args["probs"])):
-                query_args["probs"][i] = SIZE_PROB_MAPPING[params.db_name]
-        elif query_args["key"] == "_1tag_resize":
-            for i in range(len(query_args["probs"])):
-                query_args["probs"][i] = SIZE_PROB_MAPPING[params.db_name]
-        elif query_args["key"] == "_1tag_loc20_resize":
-            for i in range(len(query_args["probs"])):
-                query_args["probs"][i] = SIZE_PROB_MAPPING[params.db_name] * (0.3)
-        elif query_args["key"] == "_2tag_resize":
-            for i in range(len(query_args["probs"])):
-                query_args["probs"][i] = SIZE_PROB_MAPPING[params.db_name] * (0.8)
-        elif query_args["key"] == "_2tag_loc20_resize":
-            for i in range(len(query_args["probs"])):
-                query_args["probs"][i] = SIZE_PROB_MAPPING[params.db_name] * (0.3)
-        else:
-            for i in range(len(query_args["probs"])):
-                query_args["probs"][i] = SIZE_PROB_MAPPING[params.db_name]
-
-        if query_args["comptype"] == "and":
-            for i in range(len(query_args["probs"])):
-                query_args["probs"][i] = query_args["probs"][i] * 0.6
+        # Set probabilities so that results returned a controlled
+        # number of results
+        query = query_args["key"]
+        q_key_prob = query.replace('_resize','')
+        for i in range(len(query_args["probs"])):
+            query_args["probs"][i] = PROB_MAPPING[params.db_name][q_key_prob]
 
         print('Query:{}'.format(query_args))
 
@@ -232,9 +261,13 @@ def main(params):
             all_times.append([res['response_time'] for res in results if res])
             all_len.append(  [res['response_len']  for res in results if res])
 
-            print('ITERATION TIME: {:0.4f}s ({:0.4f} mins)\n'.format(
+            print('ITERATION TIME: {:0.4f}s ({:0.4f} mins)'.format(
                                     end_time_iteration,
                                     end_time_iteration / 60.), flush=True)
+            print('Query Time Avg: {:0.4f}s'.format(np.mean(all_times)),
+                                                    flush=True)
+            print('N Results Avg: {:0.4f} \n'.format(np.mean(all_len)),
+                                                    flush=True)
 
         print("\n")
 
--- yfcc100m/python/eval/run_performance.sh
+++ yfcc100m/python/eval/run_performance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-experiment_name=perf_metadata_8_50M
+NITER=5
+experiment_name=perf_all_threads_images
 
 running_folder=running_experiment
 info=${running_folder}/info.txt
@@ -14,11 +14,11 @@ echo $experiment_name "started..." >> $info
 
 date >> $info
 
-# for th in 2 4 8 16 32 56 64 112
-for th in 8
+for th in 2 4 8 16 32 56 64 112
+# for th in 4 8 16
 do
-    # for size in 5M 10M
-    for size in 1M 5M 10M 50M
+    # for size in 1M 5M 10M 50M
+    for size in 1M 5M 10M 50M 100M
     do
         for db in vdms mysql
         do
@@ -28,6 +28,7 @@ do
                     -db_type=$db \
                     -db_name=$size \
                     -numthreads=$th \
+                    -numiters=$NITER \
                     -out_folder=$running_folder \
                     >> $running_folder/log.log
                     2>> $running_folder/log_error.log
