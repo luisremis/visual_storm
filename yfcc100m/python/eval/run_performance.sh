cd21c3b83d6f188ea05ae3c640d28d68af5be27e
Luis Remis <luis.remis@intel.com>
2021-03-02 19:38:48 -0800

    Fix run_performance

--- yfcc100m/python/eval/run_performance.sh
+++ yfcc100m/python/eval/run_performance.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-NITER=5
-experiment_name=perf_all_threads_images
+
+NITER=10
+experiment_name=perf_results
 
 running_folder=running_experiment
 info=${running_folder}/info.txt
@@ -15,23 +16,21 @@ echo $experiment_name "started..." >> $info
 date >> $info
 
 for th in 2 4 8 16 32 56 64 112
-# for th in 4 8 16
 do
-    # for size in 1M 5M 10M 50M
     for size in 1M 5M 10M 50M 100M
     do
-        for db in vdms postgresql
+        for db in vdms mysql postgresql
         do
-            # Run VDMS Queries
             echo "Running $th $db ${size}..." >> $info
+
             python3 performance.py \
                     -db_type=$db \
                     -db_name=$size \
                     -numthreads=$th \
                     -numiters=$NITER \
                     -out_folder=$running_folder \
-                    >> $running_folder/log.log
-                    2>> $running_folder/log_error.log
+                    >>  $running_folder/log.log \
+                    2>> $running_folder/log.log
         done
     done
 done
